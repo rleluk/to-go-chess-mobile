d9574b55785b6049016e326108c40f5d68c758bb
rleluk <radoslaw.leluk@gmail.com>
2020-10-14 19:41:11 +0200

    [TOG-10] Adding TestGame component.

--- App.tsx
+++ App.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
-import {StyleSheet, View, Text} from 'react-native';
+import {StyleSheet, View} from 'react-native';
+import TestGame from './components/testGame';
 
 function App(): JSX.Element {
   return (
     <View style={styles.container}>
-      <View >
-        <Text> Hello, world! </Text>
-      </View>
+      <TestGame/>
     </View>
   );
 }
@@ -14,6 +13,8 @@ function App(): JSX.Element {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
 
